782a886d14ce3b15e0abc48cabd7265de074d823
Athul Iddya <git@iddya.com>
2018-10-08 14:06:46 -0400

    Addressing review comments

--- include/Number.h
+++ include/Number.h
@@ -1,53 +1,46 @@
 #ifndef NUMBER_H
 #define NUMBER_H
 
-#include <string>
 #include <iostream>
+#include <string>
 
-/*
- * Interface for big rational numbers
- */
-class Number {
-    public:
-        // Constructors
-        // Default number value is 0
-        Number();
+class Number
+{
+public:
+  // Constructors
+  // Default number value is 0
+  Number();
 
-        // The string will be converted to a rational number
-        Number(std::string number);
+  // The string will be converted to a rational number
+  Number(std::string number);
 
-        // Fraction of the form n/1
-        Number(int n);
+  // Destructor
+  ~Number();
 
-        // Fraction of the form n/d
-        Number(int n, int d);
+  // Override arithmetic operators
+  Number& operator+(const Number& n) const;
+  Number& operator+=(const Number& n);
+  Number& operator-(const Number& n) const;
+  Number& operator-=(const Number& n);
+  Number& operator*(const Number& n) const;
+  Number& operator*=(const Number& n);
+  Number& operator/(const Number& n) const;
+  Number& operator/=(const Number& n);
+  Number& operator^(const int n) const;
 
-        // Destructor
-        ~Number();
+  // Override comparison operators
+  bool operator<(const Number& n) const;
+  bool operator<=(const Number& n) const;
+  bool operator>(const Number& n) const;
+  bool operator>=(const Number& n) const;
+  bool operator==(const Number& n) const;
+  bool operator!=(const Number& n) const;
 
-        // Override arithmetic operators
-        Number& operator+ (const Number& n) const;
-        Number& operator+= (const Number& n);
-        Number& operator- (const Number& n) const;
-        Number& operator-= (const Number& n);
-        Number& operator* (const Number& n) const;
-        Number& operator*= (const Number& n);
-        Number& operator/ (const Number& n) const;
-        Number& operator/= (const Number& n);
-        Number& operator^ (const int n) const;
+  Number& sqrt() const;
 
-        // Override comparison operators
-        bool operator< (const Number& n) const;
-        bool operator<= (const Number& n) const;
-        bool operator> (const Number& n) const;
-        bool operator>= (const Number& n) const;
-        bool operator== (const Number& n) const;
-
-        Number& sqrt() const;
-
-        // Overriding the output and input operator
-        friend std::ostream& operator<<(std::ostream& os, const Number& n);
-        friend std::istream& operator>>(std::istream& is, Number& n);
+  // Overriding the output and input operator
+  friend std::ostream& operator<<(std::ostream& os, const Number& n);
+  friend std::istream& operator>>(std::istream& is, Number& n);
 };
 
-#endif //NUMBER_H
+#endif // NUMBER_H
