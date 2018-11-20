cdb166e294154a3b901512c4a0fdcd1beb6f5b6c
Lokesh Surya Paluri <lokeshsurya6@gmail.com>
2018-11-19 23:01:53 -0500

    Update Point2D.cpp

--- src/Point2D.cpp
+++ src/Point2D.cpp
@@ -1,91 +1,69 @@
-// PRIVATE SOURCE FILE
-
 #include "Point2D.h"
 #include "Point2DImpl.h"
 
-//class Point2D::Point2DImpl {};
-
-Point2D::Point2DStruct *getStruct()
+struct Point2D::Point2DStore 
 {
-	// return someObject->mystruct;
-}
+	Point2DImpl *implPointer;
 
-Point2D::Point2D()
-{
-	// Will NOT be used. Never create Point2D object without points.
-}
+	Point2DStore(std::string pointsString) 
+	{
+		implPointer = new Point2DImpl(pointsString);
+	}
 
-Point2D::Point2D(std::vector<RGPPoint2D> listOfPoints)
-{
-	// 1) Takes in listOfPoints
-	// 2) gets '.points' vector from struct (call *getStruct())
-	// 3) re-assign struct vector to listOfPoints
-}
+	Point2DStore(std::ifstream& file) 
+	{
+		implPointer = new Point2DImpl(file);
+	}
+};
 
 Point2D::Point2D(std::ifstream& file)
 {
-	// Emtpy
+	handle = new Point2DStore(file);
 }
 
 Point2D::Point2D(std::string listOfPoint2DString)
 {
-	// Takes in listOfPoint2DString and converts to vector<RGPPoint2D>
-	// and assigns vector to vector in struct
+	handle = new Point2DStore(listOfPoint2DString);
 }
 
 Point2D::~Point2D()
 {
-	// Assign vector in struct = null
+	delete handle->implPointer;
+	delete handle;
 }
 
 
-std::string getPointString() // Get the point as human readable ASCII string
+std::string Point2D::getPointString() 
 {
-	// Emtpy
+	return handle->implPointer->getPointString();
 }
 
-// static
 bool Point2D::isEmptyPoint()
 {
-	// Emtpy
+	return handle->implPointer->isEmptyPoint();
 }
 
-bool Point2D::operator==(const Point2D &p2d)
+bool Point2D::isValidPoint()
 {
-	// Emtpy
-}
-
-bool Point2D::operator!=(const Point2D &p2d)
-{
-	// Emtpy
-}
-
-RGPPoint2D Point2D::operator[](int index)
-{
-	// Emtpy
+	return handle->implPointer->isValidPoint();
 }
 
 int Point2D::getNumberOfPoints()
 {
-	// Emtpy
+	return handle->implPointer->getNumberOfPoints();
+}
+
+bool Point2D::operator==(const Point2D &p2d)
+{
+	//TODO
+}
+
+bool Point2D::operator!=(const Point2D &p2d)
+{
+	//TODO
 }
 
 std::vector<RGPSegment2D> Point2D::getBoundingBox()
 {
-	// Emtpy
-}
-
-bool Point2D::add(RGPPoint2D rgpp2d)
-{
-	// Emtpy
-}
-
-bool Point2D::update(int index, RGPPoint2D rgpp2d)
-{
-	// Emtpy
-}
-
-bool Point2D::remove(int index)
-{
-	// Emtpy
+	return handle->implPointer->getBoundingBox();
 }
