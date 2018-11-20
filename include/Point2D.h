8435ced14b97fdb2543d04721718e432f23a727a
Lokesh Surya Paluri <lokeshsurya6@gmail.com>
2018-11-19 23:01:36 -0500

    Update Point2D.h

--- include/Point2D.h
+++ include/Point2D.h
@@ -11,33 +11,25 @@
 
 class Point2D{
 public:
-	// Fields
-
 	// Constructors
-	Point2D();
-	Point2D(std::vector<RGPPoint2D> listOfPoints);	// Create the Point2D as a vector of RGPPoint2Ds
 	Point2D(std::ifstream& file);	// Take input from a file, convert the data into a Point2D  
 	Point2D(std::string listOfPoint2DString);	// Read the string as a vector of RGPPoint2D and form a Point2D
 	~Point2D();
 
 	// Methods
 	std::string getPointString();  // Get the point as human readable ASCII string
-	static bool isEmptyPoint();	// Checks if the Point2D object is empty
-	static bool isValidPoint();	// Checks if the Point2D object is empty
+	bool isEmptyPoint();	// Checks if the Point2D object is empty
+	bool isValidPoint();	// Checks if the Point2D object is empty
+	int getNumberOfPoints();	// Get the total number of RGPPoint2Ds listed
+
 	bool operator==(const Point2D &p2d);	// Override of operator == to check equality of two Point2Ds
 	bool operator!=(const Point2D &p2d);	// Override of operator != to check inequality of two Point2Ds
-	int getNumberOfPoints();	// Get the total number of RGPPoint2Ds listed
-	std::vector<RGPSegment2D> getBoundingBox();
-	bool add(RGPPoint2D rgpp2d);	// Adds a new RGPPoint2D to the existing Point2D object
-	bool update(int index, RGPPoint2D rgpp2d);	// Updates RGPPoint2D existing at specified index
-	bool remove(int index);	// Removes a RGPPoint2D at specified index
-	RGPPoint2D operator[](int index);	// Retrieves a RGPPoint2D at specified index
 
-	struct Point2DStruct;
-	Point2DStruct *getStruct();
-	
-	// Fields
-	Point2DStruct *impl;
+	std::vector<RGPSegment2D> getBoundingBox();
+
+private:
+	struct Point2DStore;
+	Point2DStore *handle;
 };
 
 #endif //POINT2D_H
