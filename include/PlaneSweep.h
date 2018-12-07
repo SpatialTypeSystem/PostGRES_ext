5fdb81d053178653903739fac96dd6986e894b4b
JasonCochran <j.a.cochran.cs@gmail.com>
2018-12-07 12:07:12 -0500

    Adding getEvent functions

--- include/PlaneSweep.h
+++ include/PlaneSweep.h
@@ -104,9 +104,6 @@ template<class F, class G>
 inline
 void PlaneSweep<F,G>::select_first()
 {
-    // staticIndexF = 0;
-    // staticIndexG = 0;
-
     if (!staticSequenceF.empty())
     {
         if (!staticSequenceG.empty())
@@ -271,15 +268,18 @@ inline
 //start implemnting iterators
 F PlaneSweep<F,G>::getEventF()
 {
-
-
+    // Get event returns the element to which the logical pointer of a
+    // point or half segment sequence of an object points to
+    return (*staticIteratorF);
 }
 
 template <class F, class G>
 inline
 G PlaneSweep<F,G>::getEventG()
 {
-
+    // Get event returns the element to which the logical pointer of a
+    // point or half segment sequence of an object points to
+    return (*staticIteratorG);
 }
 
 template <class F, class G>
@@ -300,48 +300,53 @@ template <class F, class G>
 inline
 void PlaneSweep<F,G>::insert(RGPSegment2D s)
 {
-
+    // Inserts/Removes a segment from the sweep line status when
+    // it's left/right half segment is encountered
 }
 
 template <class F, class G>
 inline
 void PlaneSweep<F,G>::remove(RGPSegment2D s)
 {
-
+    // Inserts/Removes a segment from the sweep line status when
+    // it's left/right half segment is encountered
 }
 
 template <class F, class G>
 inline
 bool PlaneSweep<F,G>::lookAheadF(RGPHalfSegment2D h)
 {
-
+    // Check if the dominant points of the given halfsegment and the next
+    // halfsegment (after the current index) of the indicated sequence are equal
 }
 
 template <class F, class G>
 inline
 bool PlaneSweep<F,G>::lookAheadG(RGPHalfSegment2D h)
 {
-
+    // Check if the dominant points of the given halfsegment and the next
+    // halfsegment (after the current index) of the indicated sequence are equal
 }
 
 template <class F, class G>
 inline
 optional<bool> PlaneSweep<F,G>::getAttributeOfSegmentBelow(RGPPoint2D p)
 {
-
+    // Returns the nearest annotated halfsegment, from the sweep line status,
+    // below a given point, if there is one
 }
 
 template <class F, class G>
 inline
 optional<std::tuple<short, short>> PlaneSweep<F,G>::getOverlapNumbersOfPredecessor(RGPSegment2D s)
 {
-
+    // Returns the overlap numbers of the predecessor to the given segment in the sweep line status
 }
 
 template <class F, class G>
 inline
 optional<std::tuple<short, short>> PlaneSweep<F,G>::getOverlapNumbersOf(RGPSegment2D s)
 {
-
+    // Returns the overlap numbers of the given segment in the sweep line status
 }
 #endif //PLANESWEEP
