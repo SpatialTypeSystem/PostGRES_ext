b108e368e011b887331451341cf8fa8c5b757667
JasonCochran <j.a.cochran.cs@gmail.com>
2018-12-08 12:36:59 -0500

    Initial spatial operations attempt

--- include/SpatialOperations.h
+++ include/SpatialOperations.h
@@ -1,11 +1,18 @@
 #ifndef SPATIALOPERATIONS_H
 #define SPATIALOPERATIONS_H
 
+#include "STO.h"
+
 class SpatialOperations{
 public:
 	SpatialOperations();
 	~SpatialOperations();
 
+	Point2D union(Point2D F, Point2D G);
+	Point2D intersection(Point2D F, Point2D G);
+	Line2D intersection(Line2D F, Line2D G)
+	Region2D intersection(Region2D F, Region2D G);
+	Line2D intersection(Region2D F, Line2D G);
 private:
 	
 };
--- src/SpatialOperations.cpp
+++ src/SpatialOperations.cpp
@@ -5,7 +5,22 @@ SpatialOperations::SpatialOperations()
 	// Emtpy
 }
 
-SpatialOperations::~SpatialOperations(std::vector<RGPPoint2D> listOfPoints)
+SpatialOperations::~SpatialOperations()
 {
 	// Emtpy
+}
+
+Point2D SpatialOperations::union(Point2D F, Point2D G) {
+	Point2D poi = new Point2D();
+	// Point2D won't accept duplicate points being added
+	// Loop through both sets of points and add them to a new point object
+	for (int i = 0; i < F.getNumberOfPoints(); i++ ) {
+		poi.add( F[i] );
+	}
+	
+	for (int i = 0; i < G.getNumberOfPoints(); i++ ) {
+		poi.add( G[i] );
+	}
+	
+	return poi;
 }
