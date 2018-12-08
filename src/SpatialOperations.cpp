b50abd0dd2dbe052a7cc1cad6faf913317a72f90
JasonCochran <j.a.cochran.cs@gmail.com>
2018-12-08 12:43:20 -0500

    Adding all functions for spatial operations

--- src/SpatialOperations.cpp
+++ src/SpatialOperations.cpp
@@ -17,10 +17,49 @@ Point2D SpatialOperations::union(Point2D F, Point2D G) {
 	for (int i = 0; i < F.getNumberOfPoints(); i++ ) {
 		poi.add( F[i] );
 	}
-	
+
 	for (int i = 0; i < G.getNumberOfPoints(); i++ ) {
 		poi.add( G[i] );
 	}
 	
 	return poi;
+}
+
+Point2D union(Point2D F, Point2D G) {
+	return new Point2D();
+}
+Line2D union(Line2D F, Line2D G) {
+	return new Line2D();
+}
+
+Region2D union(Region2D F, Region2D G) {
+	return new Region2D();
+}
+
+Point2D intersection(Point2D F, Point2D G) {
+	return new Point2D();
+}
+
+Line2D intersection(Line2D F, Line2D G) {
+	return new Line2D();
+}
+
+Region2D intersection(Region2D F, Region2D G) {
+	return new Region2D();
+}
+
+Line2D intersection(Region2D F, Line2D G) {
+	return new Line2D();
+}
+
+Point2D difference(Point2D F, Point2D G) {
+	return new Point2D();
+}
+
+Line2D difference(Line2D F, Line2D G) {
+	return new Line2D();
+}
+
+Region2D difference(Region2D F, Region2D G) {
+	return new Region2D();
 }
