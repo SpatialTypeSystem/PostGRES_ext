6149faa64ce588ba66b9780d38596dea8b9da734
JasonCochran <j.a.cochran.cs@gmail.com>
2018-12-08 17:16:53 -0500

    More spatial predicate tests

--- test/SpatialPredicateTests.cpp
+++ test/SpatialPredicateTests.cpp
@@ -1,35 +1,63 @@
 #include "Relationship.h"
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE SpatialPredicateTests
-#include <boost/test/output_test_stream.hpp>
-#include <boost/test/unit_test.hpp>
+
 #include <iostream>
 #include <vector>
+#include <string>
 
-BOOST_AUTO_TEST_CASE(lines_disjoint)
+void printResultMessage( std::string testname, bool result) {
+	std::cout << std::boolalpha;
+	std::cout << testname << result;
+}
+
+void point_disjoint_false()
 {
 	// Check if we lines are disjoint
-	bool result = disjoint(  );
-	BOOST_REQUIRE( result, false);
+	bool result = disjoint(Point2D("((1,2),(2,3))"), Point2D("((1,2),(2,3))") );
+	printResultMessage("point_disjoint_false", result == false);
 }
 
-BOOST_AUTO_TEST_CASE(regions_overlap)
+void point_disjoint_true()
+{
+	// Check if we lines are disjoint
+	bool result = disjoint(Point2D("((1,2),(2,3))"), Point2D("((2,2),(3,3))"));
+	printResultMessage("point_disjoint_false", result == true);
+}
+
+void regions_overlap_true()
 {
 	// Check if 2 regions overlap
-	bool result = disjoint(  );
-	BOOST_REQUIRE( result, true);
+	bool result = disjoint( Region2D("[WCR((2 2,2 4,4 4,4 2,2 2))]"), Region2D("[WCR((1 1,1 3,3 3,3 1,1 1))]") );
+	printResultMessage("region_covers", result == true);
 }
 
-BOOST_AUTO_TEST_CASE(line_equal)
+void line_equal_true()
 {
-	// Check if 2 lines equal
-	bool result = equal(  );
-	BOOST_REQUIRE( result, true);
+	bool result = equal( Line2D("( (3,4),(2,2) )"), Line2D("( (3,4),(2,2) )"));
+	printResultMessage("region_covers", result == true);
 }
 
-BOOST_AUTO_TEST_CASE(region_covers)
+void line_equal_false()
+{
+	bool result = equal( Line2D("( (3,4),(2,2) )"), Line2D("( (4,5),(2,2) )"));
+	printResultMessage("region_covers", result == false);
+}
+
+void region_covers_true()
 {
 	// Check if 2 regions cover
-	bool result = covers(  );
-	BOOST_REQUIRE( result, false);
+	bool result = covers( Region2D("[WCR((0 0,0 6,6 6,6 0,0 0))]"), Region2D("[WCR((1 1,1 2,2 2,2 1,1 1))]") );
+	printResultMessage("region_covers", result == true);
+}
+
+void region_covers_false()
+{
+	// Check if 2 regions cover
+	bool result = covers( Region2D("[WCR((1 1,1 2,2 2,2 1,1 1))]"), Region2D("[WCR((0 0,0 6,6 6,6 0,0 0))]") );
+	printResultMessage("region_covers", result == false);
+}
+
+void determine_overlap_true()
+{
+	PREDICATE result = determine( Region2D("[WCR((2 2,2 4,4 4,4 2,2 2))]"), Region2D("[WCR((1 1,1 3,3 3,3 1,1 1))]") );
+	printResultMessage("region_covers", PREDICATE.determine == result);
 }
