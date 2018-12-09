ab939e918f84aea63017c094a3c76b506aa1688a
Lokesh Surya Paluri <lokeshsurya6@gmail.com>
2018-12-09 07:47:53 -0500

    Update Point2DForProgrammer.h

--- include/Point2DForProgrammer.h
+++ include/Point2DForProgrammer.h
@@ -12,14 +12,31 @@
 class Point2DForProgrammer{
 public:
 	// Constructors
-	Point2DForProgrammer(std::ifstream& file);	// Take input from a file, convert the data into a Point2DForProgrammer  
+	Point2DForProgrammer(std::vector<RGPPoint2D>);	// Take input from a file, convert the data into a Point2DForProgrammer  
 	Point2DForProgrammer(std::string listOfPoint2DString);	// Read the string as a vector of RGPPoint2D and form a Point2DForProgrammer
 	~Point2DForProgrammer();
 
+	//iterator
+	class iterator
+	{
+		public:
+			iterator(RGPPoint2D*);
+			RGPPoint2D operator*();
+			RGPPoint2D operator++(int);
+			RGPPoint2D operator++();
+			bool operator!=(const iterator&);
+			bool operator==(const iterator&);
+			RGPPoint2D *ptr;
+	};
+
+	iterator begin();
+	iterator end();
+
+
 	// Methods
-	bool add(RGPPoint2D rgpp2d);	// Adds a new RGPPoint2D to the existing Point2DForProgrammer object
-	bool update(int index, RGPPoint2D rgpp2d);	// Updates RGPPoint2D existing at specified index
-	bool remove(int index);	// Removes a RGPPoint2D at specified index
+	bool add(RGPPoint2D rgpp2d);	// Adds a new RGPPoint2D to the existing Point2D object
+	bool update(iterator it, RGPPoint2D rgpp2d);	// Updates RGPPoint2D existing at specified index
+	bool remove(iterator it);	// Removes a RGPPoint2D at specified index
 	//RGPPoint2D operator[](int index);	// Retrieves a RGPPoint2D at specified index
 
 private:
