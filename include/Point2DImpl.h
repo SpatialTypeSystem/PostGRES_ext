c5fffd26dcc462206aa7a4e1d60b78523dc7ad83
Lokesh Surya Paluri <lokeshsurya6@gmail.com>
2018-12-09 07:05:59 -0500

    Update Point2DImpl.h

--- include/Point2DImpl.h
+++ include/Point2DImpl.h
@@ -9,9 +9,9 @@
 class Point2DImpl
 {
 	public:
-		Point2DImpl(std::vector<RGPPoint2D> pointVector);
 		Point2DImpl();
 		Point2DImpl(std::ifstream& file);	// Take input from a file, convert the data into a Point2D  
+		Point2DImpl(std::vector<RGPPoint2D> pointVector);
 		Point2DImpl(std::string listOfPoint2DString);	// Read the string as a vector of RGPPoint2D and form a Point2D
 		~Point2DImpl();
 		class iterator
@@ -32,9 +32,10 @@ class Point2DImpl
 		bool isEmptyPoint();	// Checks if the Point2D object is empty
 		bool isValidPoint();	// Checks if the Point2D object is empty
 		int getNumberOfPoints();	// Get the total number of RGPPoint2Ds listed
+		std::vector<RGPPoint2D> getVectorOfPoints();
 		iterator begin();  //returns iterator to the first point
 		iterator end();   // return iterator to the last point
-		Line2DImpl getBoundingBox(); //returns the bounding box of the set od points	
+		RGPSegment2D getBoundingBox(); //returns the bounding box of the set od points	
 
 		bool add(RGPPoint2D rgpp2d);	// Adds a new RGPPoint2D to the existing Point2D object
 		bool update(iterator it, RGPPoint2D rgpp2d);	// Updates RGPPoint2D existing at specified index
