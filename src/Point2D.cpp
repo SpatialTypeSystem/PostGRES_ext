3a1499e237fa9d84b95d1fdf91d5cea54bfecc28
Lokesh Surya Paluri <lokeshsurya6@gmail.com>
2018-12-09 04:39:40 -0500

    Update Point2D.cpp

--- src/Point2D.cpp
+++ src/Point2D.cpp
@@ -16,6 +16,57 @@ struct Point2D::Point2DStore
 	}
 };
 
+/*
+Point2DImpl::iterator::iterator(RGPPoint2D *ptr1)
+{
+	ptr = ptr1;
+}
+
+RGPPoint2D Point2DImpl::iterator::operator*()
+{
+	return *ptr;
+}
+
+RGPPoint2D Point2DImpl::iterator::operator++(int junk)
+{
+	RGPPoint2D *ptr1;
+	ptr1 = ptr;
+	ptr++;
+	return *ptr1;
+}
+
+RGPPoint2D Point2DImpl::iterator::operator++()
+{
+	ptr++;
+	return *ptr;
+}
+
+bool Point2DImpl::iterator::operator!=(const iterator &it)
+{
+	if(it.ptr==ptr)
+		return false;
+	return true;
+}
+bool Point2DImpl::iterator::operator==(const iterator &it)
+{
+	if(it.ptr!=ptr)
+		return false;
+	return true;
+}
+
+Point2DImpl::iterator Point2DImpl::begin()
+{
+	//auto it = handle->implPointer->begin();
+	//RGPPoint2D *ptr = &(handle->vectorOfPoints[0]);
+	//return iterator(&(*it));
+}
+
+Point2DImpl::iterator Point2DImpl::end()
+{
+	//int t = handle->vectorOfPoints.size();
+	//return (iterator(&(handle->vectorOfPoints[t-1])));
+}
+*/
 Point2D::Point2D(std::ifstream& file)
 {
 	handle = new Point2DStore(file);
