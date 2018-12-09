c38444b27aff1aa7a982a09285ec078019bbf064
Lokesh Surya Paluri <lokeshsurya6@gmail.com>
2018-12-09 04:39:43 -0500

    Update Point2DImpl.cpp

--- src/Point2DImpl.cpp
+++ src/Point2DImpl.cpp
@@ -1,25 +1,38 @@
 #include "Point2DImpl.h"
 #include "RGPPoint2D.h"
+#include "RGP.h"
+#include "RGPSegment2D.h"
+#include "RGPHalfSegment2D.h"
+#include "Line2DImpl.h"
 #include <Number.h>
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <exception>
 
-
+//creates vectors to store points and bounding
 struct Point2DImpl::Point2DImplStore 
 {
 	std::vector<RGPPoint2D> vectorOfPoints;
 	std::vector<RGPPoint2D> boundingBox;
 };
 
+//iterator to iterate through our data structure and return elements accordingly.
+//constructor to the iterator, points iterator to a particular element of our data type. 
 Point2DImpl::iterator::iterator(RGPPoint2D *ptr1)
 {
 	ptr = ptr1;
 }
 
+//overloading * operator to return the pointing element
 RGPPoint2D Point2DImpl::iterator::operator*()
 {
 	return *ptr;
 }
 
+//overloading ++ operator to move the iterator through our set of points
+//this method is for postincrement
 RGPPoint2D Point2DImpl::iterator::operator++(int junk)
 {
 	RGPPoint2D *ptr1;
@@ -28,18 +41,23 @@ RGPPoint2D Point2DImpl::iterator::operator++(int junk)
 	return *ptr1;
 }
 
+//overloading ++ operator to move the iterator through our set of points
+//this method is for preincrement
 RGPPoint2D Point2DImpl::iterator::operator++()
 {
 	ptr++;
 	return *ptr;
 }
 
+//checks if both the iterators are not equal.
 bool Point2DImpl::iterator::operator!=(const iterator &it)
 {
 	if(it.ptr==ptr)
 		return false;
 	return true;
 }
+
+//checks if both the iterators are equal.
 bool Point2DImpl::iterator::operator==(const iterator &it)
 {
 	if(it.ptr!=ptr)
@@ -47,12 +65,14 @@ bool Point2DImpl::iterator::operator==(const iterator &it)
 	return true;
 }
 
+//method for pointImpl, used mainly with iterators. returns iterator to the first point in the list of Points.
 Point2DImpl::iterator Point2DImpl::begin()
 {
 	RGPPoint2D *ptr = &(handle->vectorOfPoints[0]);
 	return iterator(ptr);
 }
 
+//method for pointImpl, used mainly with iterators. returns iterator to the last point in the list of Points.
 Point2DImpl::iterator Point2DImpl::end()
 {
 	int t = handle->vectorOfPoints.size();
@@ -60,40 +80,48 @@ Point2DImpl::iterator Point2DImpl::end()
 }
 
 //Constructors
+//constructor to initialise point with a vector of RGPPoints.
 Point2DImpl::Point2DImpl(std::vector<RGPPoint2D> listOfPoints)
 {
 	handle = new Point2DImplStore;
 	handle->vectorOfPoints = listOfPoints;
 }
 
+//this constructor does not have any args. This just re initializes the handle.
 Point2DImpl::Point2DImpl()
 {
 	handle = new Point2DImplStore;
 }
 
+//constructor to read points from a file
 Point2DImpl::Point2DImpl(std::ifstream& file)
 {
-	/*std::string inputString;
+	std::string inputString;
 	if(file.is_open())
 	{
 		std::stringstream strBuffer;
 		strBuffer << file.rdbuf();
-		inputString = strBuffer;
+		inputString = strBuffer.str();
 	}
 	else
 	{
-		throw std::exception("Error while reading the file");
+		throw std::runtime_error("Error while reading the file");
 	}
-
-	//TODO
-	vectorOfPoints = parseStringToVectorOfPoints(inputString);
-	*/
+	
+	if(parseStringToVectorOfPoints(inputString))
+		std::cout << "success" << std::endl;
+	else{
+		std::cout << "failed" << std::endl;
+		handle->vectorOfPoints.clear();
+	}
 }
 
+
+//This constructor creates a Point2DImpl object based on a passed in string of points. We follow a specific convention here.
+//((a,b),(c,d))
 Point2DImpl::Point2DImpl(std::string listOfPoint2DString)
 {
 	handle = new Point2DImplStore;
-	std::cout<< "inside" << "\n";
 	if(parseStringToVectorOfPoints(listOfPoint2DString))
 	{
 		std::cout<<"All points are added successfully"<<std::endl;
@@ -101,6 +129,8 @@ Point2DImpl::Point2DImpl(std::string listOfPoint2DString)
 	else
 	{
 		std::cout<<"Input Format is not correct"<<std::endl;
+		handle->vectorOfPoints.clear();
+
 	}
 }
 
@@ -110,6 +140,7 @@ Point2DImpl::~Point2DImpl()
 	//default;
 }
 
+//this method checks if the structure is empty
 bool Point2DImpl::isEmptyPoint()
 {
 	return handle->vectorOfPoints.empty();
@@ -139,6 +170,7 @@ std::string Point2DImpl::getPointString()
 	return resultString;
 }
 
+//method to print all points in the Point2D object
 void Point2DImpl::printAllPoints()
 {
 	std::cout<<"(";
@@ -160,19 +192,45 @@ bool Point2DImpl::isValidPoint()
 	return validity;
 }
 
+//this method return total number of points in the Point2D object.
 int Point2DImpl::getNumberOfPoints()
 {
 	return handle->vectorOfPoints.size();
 }
 
-Point2DImpl Point2DImpl::getBoundingBox()
+//returns bounding box for the object
+Line2DImpl Point2DImpl::getBoundingBox()
 {
-	std::vector<RGPPoint2D> box;
-	box = handle->boundingBox;
-	Point2DImpl pt(box);
-	return pt;
+	std::vector<RGPPoint2D> points;
+	std::vector<RGPPoint2D> boundPoints;
+	//get all the points in the object
+	points = handle->vectorOfPoints;
+
+	//get the least point to get the first point for diagonal
+	boundPoints.push_back(points[0]);
+
+	//get the maxy for the secound point
+	auto maxy = points[0].y;
+	for(auto it = points.begin(); it!=points.end() ;it++)
+	{
+		if((*it).y > maxy)
+			maxy=(*it).y;
+	}
+	//make the secound point from the set of points
+	RGPPoint2D pt(points[points.size() - 1].x,maxy);
+	//push back the secound point
+	boundPoints.push_back(pt);
+
+	//create the diagonal
+	RGPSegment2D seg(boundPoints[0],boundPoints[1]);
+	std::vector<RGPSegment2D> segs;
+
+	segs.push_back(seg);
+	Line2DImpl ln(segs);
+	return ln;
 }
 
+//method checks if the both Point2D objects are equal
 bool Point2DImpl::operator==(const Point2DImpl &p2d)
 {
 	int i=0;
@@ -189,6 +247,7 @@ bool Point2DImpl::operator==(const Point2DImpl &p2d)
 		return false;
 }
 
+//method checks if the both Point2D objects are not equal
 bool Point2DImpl::operator!=(const Point2DImpl &p2d)
 {
 	int i=0;
@@ -204,6 +263,7 @@ bool Point2DImpl::operator!=(const Point2DImpl &p2d)
 	return false;
 }
 
+//returns the point at that particular index of the Point2D object
 Point2DImpl Point2DImpl::operator[](int index)
 {
 	std::vector<RGPPoint2D> t;
@@ -212,19 +272,21 @@ Point2DImpl Point2DImpl::operator[](int index)
 	return temp;
 }
 
+//assigns the object to the right to the object in the left.
 Point2DImpl Point2DImpl::operator=(const Point2DImpl &p2d)
 {
 	handle->vectorOfPoints.clear();
 	handle->vectorOfPoints = p2d.handle->vectorOfPoints;
 }
 
+//this method is used to add a new point to Point2D object
 bool Point2DImpl::add(RGPPoint2D rgpp2d)
 {
-	
-	//handle->vectorOfPoints.push_back(rgpp2d);
 	try
     {
-		if(handle->vectorOfPoints.empty()) {
+		//check if the vector is empty
+		if(handle->vectorOfPoints.empty()) 
+		{
 			handle->vectorOfPoints.push_back(rgpp2d);
 			return true;
 		}
@@ -244,6 +306,7 @@ bool Point2DImpl::add(RGPPoint2D rgpp2d)
 	return true;
 }
 
+//this method is used to update a point at a particular index
 bool Point2DImpl::update(Point2DImpl::iterator it, RGPPoint2D rgpp2d)
 {
 	try
@@ -267,6 +330,7 @@ bool Point2DImpl::update(Point2DImpl::iterator it, RGPPoint2D rgpp2d)
     return true;
 }
 
+//removes a particular point in the vector of points
 bool Point2DImpl::remove(Point2DImpl::iterator it)
 {
 	try
@@ -288,6 +352,7 @@ bool Point2DImpl::remove(Point2DImpl::iterator it)
     return true;
 }
 
+//merge sort imlementation to sort all points
 void Point2DImpl::pointSort(std::vector<RGPPoint2D> &bar)
 {
 	if (bar.size() <= 1) 
@@ -337,6 +402,7 @@ void Point2DImpl::mergeSort(std::vector<RGPPoint2D> &left, std::vector<RGPPoint2
     }
 }
 
+//parse method to parse all points from the string and make the vector of points
 bool Point2DImpl::parseStringToVectorOfPoints(std::string st)
 {
 	std::vector<RGPPoint2D> points;
@@ -344,13 +410,17 @@ bool Point2DImpl::parseStringToVectorOfPoints(std::string st)
 	std::string num1,num2;
 	std::string s =st;
 	std::vector<Number> nums;
+
+	//initialize the delimiter
 	std::string delimeter = ",";
+
+	//erase first and last (..*)
 	s.erase(0,1);
 	s.erase(s.length()-1,1);
   	s.append(",");
-	std::cout << s << "\n";
+	  
+	//keep finding the substrings 
 	pos = s.find(delimeter);
-	std::cout << pos << "\n";
 	try{
 		while(pos!= std::string::npos)
 		{
@@ -361,6 +431,7 @@ bool Point2DImpl::parseStringToVectorOfPoints(std::string st)
 				{
 					if(a[0]=='(')
 					{
+						//get the complete number and store it.
 						std::string temp = "";
 						int i =1;
 						while(((a[i]<='9'&&a[i]>='0')||a[i]=='.'||a[i]=='-')&&i<a.length())
@@ -369,9 +440,12 @@ bool Point2DImpl::parseStringToVectorOfPoints(std::string st)
 							i++;
 						} 
 						num1=temp;
+						fl++;
 					}
-					else if(a[a.length()-1]==')')
+					else if(a[a.length()-1]==')'&&fl==1)
 					{
+						//get the secound number
+						fl=0;
 						std::string temp = "";
 						int i =0;
 						while(((a[i]<='9'&&a[i]>='0')||a[i]=='.'||a[i]=='-')&&i<a.length()-1)
@@ -380,44 +454,34 @@ bool Point2DImpl::parseStringToVectorOfPoints(std::string st)
 							i++;
 						} 
 						num2=temp;
-            			std::cout<<"num1 and num2 is "<<num1<<" "<<num2<<std::endl;	//numbers from points
+						//make RGPPoints and push back into the points vector
 						points.push_back(RGPPoint2D(Number(num1),Number(num2)));
 					}
 					else
+					{
+						//return false if the input string is corrupt	
 						return false;
+					}
 				}
 				catch(int e){
+					//return false if the input string is corrupt
 					return false;
 				}
 			}
 			else
 			{
+				//return false if the input string is corrupt
 				return false;
 			}
+			//keep erasing the strings till we get our delimiter
 			s.erase(0, pos + delimeter.length());
-      		std::cout <<"string s is"<< s << "\n";
 			pos = s.find(delimeter);
 		}
 	}
 	catch(int e){
 		return false;
 	}
+	//sort all points and add it to the vector
 	pointSort(points);
-
-	std::vector<RGPPoint2D> boundPoints;
-	boundPoints.push_back(points[0]);
-	auto maxy = points[0].y;
-	std::cout<<"bound box \n";
-	for(auto it = points.begin(); it!=points.end() ;it++)
-	{
-		if((*it).y > maxy)
-			maxy=(*it).y;
-	}
-	RGPPoint2D pt(points[points.size() - 1].x,maxy);
-	boundPoints.push_back(pt);
-	
-	std::cout<<std::endl;
 	handle->vectorOfPoints = points;
-	handle->boundingBox = boundPoints;
-	
 }
