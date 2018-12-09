5a72a61adb3f2a4161a50f4fda555c8b29abc39b
Lokesh Surya Paluri <lokeshsurya6@gmail.com>
2018-12-09 04:39:29 -0500

    Update Region2DImpl.h

--- include/Region2DImpl.h
+++ include/Region2DImpl.h
@@ -10,31 +10,59 @@ class Region2DImpl
 {
 public:
 	// Constructors
-	Region2DImpl(std::vector<RGPAnnotatedHalfSegment2D> listOfRegions);
-	Region2DImpl();
 	Region2DImpl(std::string listOfRegion2DString);
-	Region2DImpl(std::ifstream& file); // Send in file for constructor (possibly .txt)
 	~Region2DImpl();
 
+	class iteratorforFaces
+	{
+		public:
+			iteratorforFaces(std::vector<RGPAnnotatedHalfSegment2D>*);
+			std::vector<RGPAnnotatedHalfSegment2D> operator*();
+			std::vector<RGPAnnotatedHalfSegment2D> operator++(int);
+			std::vector<RGPAnnotatedHalfSegment2D> operator++();
+			bool operator!=(const iteratorforFaces&);
+			bool operator==(const iteratorforFaces&);
+			std::vector<RGPAnnotatedHalfSegment2D> *ptr;
+	};
+
+	class iteratorforHoles
+	{
+		public:
+			iteratorforHoles(std::vector<RGPAnnotatedHalfSegment2D>*);
+			std::vector<RGPAnnotatedHalfSegment2D> operator*();
+			std::vector<RGPAnnotatedHalfSegment2D> operator++(int);
+			std::vector<RGPAnnotatedHalfSegment2D> operator++();
+			bool operator!=(const iteratorforHoles&);
+			bool operator==(const iteratorforHoles&);
+			std::vector<RGPAnnotatedHalfSegment2D> *ptr;
+	};
+
+	iteratorforHoles beginHoles();
+	iteratorforHoles endHoles();
+	iteratorforFaces beginFaces();
+	iteratorforFaces endFaces();
+
 	// Methods
-	std::string getRegionString();                          // Get the region as human readable ASCII string
 	Number area();
-	bool isEmptyRegion(Region2DImpl region);
-	bool isValidRegion(Region2DImpl region);
+	bool isEmptyRegion();
 	int getNumberOfFaces();
-	std::vector<RGPSegment2D> getBoundingBox();
+	int getNumberOfHoles();
 	bool operator==(const Region2DImpl &p2d);
 	bool operator!=(const Region2DImpl &p2d);
-	std::vector<RGPSegment2D> getCycle(int index);
-	std::vector<RGPAnnotatedHalfSegment2D> getFace(int index);
 	bool addFace(std::vector<RGPSegment2D>);
-	bool update(int index, std::vector<RGPSegment2D>);	// Updates a whole region at specified index
-	bool remove(int index);	                                // Removes a region at specified index
-	std::vector<RGPSegment2D> operator[](int index);	// Retrieves a region at specified index
+	bool removeFace(int index);	                                // Removes a region at specified index
+	RGPSegment2D getBoundingBox();
 
 private:
 	class Region2DImplStore;
+	Number computeArea();
+	void sortAnnotatedHS(std::vector<RGPAnnotatedHalfSegment2D> vec);
+	void mergeSort(std::vector<RGPAnnotatedHalfSegment2D> &left, std::vector<RGPAnnotatedHalfSegment2D> &right, std::vector<RGPAnnotatedHalfSegment2D> &bars);
 	bool parseWDR(std::string inputString);
+	Number computeDirection(RGPPoint2D pi, RGPPoint2D pj, RGPPoint2D pk); 
+	bool doSegmentsIntersect(RGPSegment2D a, RGPSegment2D b);
+	bool onSegment(RGPPoint2D pi, RGPPoint2D pj, RGPPoint2D pk);
+	bool validateRegion();
 	Number getAreaOfCycle(std::vector<RGPAnnotatedHalfSegment2D> vectorOfSegments);
 	Region2DImplStore *handle;
 	std::vector<RGPAnnotatedHalfSegment2D> constructRegion(std::string formattedInputString);
